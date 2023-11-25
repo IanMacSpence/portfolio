7c583f3ea8d6d2fa2f1500a0939eb238c136dd74
Ian MacDonald Spence <ianmacdonaldspence@Ians-MacBook-Pro.local>
2023-11-25 02:33:41 +0200

    Adjust themes and modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,13 @@
 function switchTheme(themeName) {
-  // Assuming 'theme' is a class that denotes the theme
-  // Remove all other theme classes first
+
   const themeClasses = [
-    'theme-french-blue', 'theme-cornflower-blue', 'theme-metallic-seaweed',
-    'theme-teal2', 'theme-cerulean', 'theme-hookers-green',
-    'theme-bottle-green', 'theme-forest-green', 'theme-myrtle-green'
+    'theme-hookers-green', 
+    'theme-french-blue', 
+    'theme-red', 
+    'theme-orange'
   ];
 
-  // Get the element that you want to apply the themes to, for example, body
-  const themedElement = document.body; // or document.documentElement for <html> element
-
-  // Remove all existing theme classes
+  const themedElement = document.body;
   themedElement.classList.remove(...themeClasses);
-
-  // Add the selected theme class
   themedElement.classList.add(themeName);
 }
